ad3dccda3a6a610340eeafd1667148eb27e01584
José Fonseca <jrfonseca@users.sourceforge.net>
2006-03-14 10:40:03 +0000

    Add a 'map' builtin to the base walker class.

--- walker.py
+++ walker.py
@@ -15,3 +15,16 @@ class Walker:
 
 	def __apply__(self, root):
 		raise NotImplementedError
+		
+	def map(self, target, func, *args):
+		if target.getType() != aterm.LIST:
+			raise Failure
+
+		if target.isEmpty():
+			return target
+
+		return self.factory.makeConsList(
+				func(target.getHead(), *args), 
+				self.map(target.getTail(), func, *args)
+			)
+		
