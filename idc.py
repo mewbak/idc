542cbb1469eb38c809f7975cee0a67b9e9c7f4b8
José Fonseca <jrfonseca@users.sourceforge.net>
2006-03-24 17:27:33 +0000

    Open file.

--- idc.py
+++ idc.py
@@ -8,6 +8,8 @@ locale.setlocale(locale.LC_ALL,'')
 
 import gettext
 
+import os.path
+
 
 class GladeWindow:
 	"""
@@ -52,16 +54,71 @@ class GladeWindow:
 class GladeApp(GladeWindow):
 
 	def main(self):
+		"""Enter main loop."""
+
 		gtk.main()
 
 	def quit(self, *args):
+		"""Quit main loop."""
+
 		gtk.main_quit()
 				
+	def open(self, title = None, parent = None, filters = None, folder = None):
+		"""Display a file open dialog."""
+
+		# See http://www.pygtk.org/pygtk2tutorial/sec-FileChoosers.html
+
+		dialog = gtk.FileChooserDialog(
+				title,
+				parent,
+				gtk.FILE_CHOOSER_ACTION_OPEN,
+				(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK),
+		)
+		dialog.set_default_response(gtk.RESPONSE_OK)
+
+		if filters is not None:
+			for name, patterns in filters:
+				filter = gtk.FileFilter()
+				filter.set_name(name)
+				for pattern in patterns:
+					filter.add_pattern(pattern)
+				dialog.add_filter(filter)
+
+		if folder is not None:
+				dialog.set_current_folder(os.path.abspath(folder))
+
+		response = dialog.run()
+		if response == gtk.RESPONSE_OK:
+			path = dialog.get_filename()
+		else:
+			path = None
+
+		dialog.destroy()
+		return path
+
 
 class MainApp(GladeApp):
 
 	def __init__(self):
 		GladeApp.__init__(self, "./idc.glade", "main_window")
+
+	def on_open_activate(self, event):
+		path = self.open(
+				None, 
+				self.widget, 
+				[
+					('Assembly Files', ['*.s']),
+					('All Files', ['*']),
+				], 
+				'./examples',
+		)
+		
+		if path is not None:
+			# See http://www.pygtk.org/pygtk2tutorial/sec-TextViews.html
+			
+			text = file(path, 'rt').read()
+			textbuffer = self.textview.get_buffer()
+			textbuffer.set_text(text)
 
 	def on_quit_activate(self, event):
 		self.quit()
