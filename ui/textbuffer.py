e381c7a065dd726a4287e4bf9d942ba8505b58da
José Fonseca <jrfonseca@users.sourceforge.net>
2006-05-13 22:18:12 +0000

    User color tags.

--- ui/textbuffer.py
+++ ui/textbuffer.py
@@ -14,6 +14,27 @@ class TextBufferFormatter(box.Formatter):
 		
 		self.buffer.set_text("", 0)
 		self.iter = self.buffer.get_iter_at_offset(0)
+		self.tag = self.buffer.create_tag()
 		
 	def write(self, s):
-		self.buffer.insert(self.iter, s)
+		self.buffer.insert_with_tags(self.iter, s, self.tag)
+
+	types = {
+		'operator': 'red',
+		'keyword': 'blue',
+		'symbol': 'black',
+		'literal': 'green',
+	}
+	
+	def handle_tag_start(self, name, value):
+		if name == 'type':
+			try:
+				color = self.types[value]
+			except KeyError:
+				color = 'black'
+			self.tag = self.buffer.create_tag(None, foreground=color)
+
+	def handle_tag_end(self, name):
+		if name == 'type':
+			color = 'black'
+			self.tag = self.buffer.create_tag(None, foreground=color)
