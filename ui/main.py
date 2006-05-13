a2da0ce82d133bf5fcafbe892098ce7692063e3e
José Fonseca <jrfonseca@users.sourceforge.net>
2006-05-13 13:48:32 +0000

    Split common glade utility classes.

--- ui/main.py
+++ ui/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+"""Main window."""
 
 # See:
 # - http://www.pygtk.org/pygtk2tutorial/
@@ -10,101 +10,17 @@ import gtk.glade
 import locale
 locale.setlocale(locale.LC_ALL,'')
 
-import gettext
-
-import os.path
-
+import glade
 
 import aterm
 import ir
 import box
 
 
-class GladeWindow:
-	"""Base class for glade based windows."""
-
-	# See: 
-	# - http://www.jamesh.id.au/software/libglade/
-	# - http://www.pygtk.org/pygtk2reference/class-gladexml.html
-	# - http://www.pixelbeat.org/libs/libglade.py
-
-	def __init__(self, filename, windowname):
-		"""Load glade file."""
-
-		self.xml = gtk.glade.XML(filename, windowname, gettext.textdomain())
-
-		#handlers = {}
-		#for name in dir(self.__class__):
-		#	handlers[name] = getattr(self, name)
-		#self.xml.signal_autoconnect(handlers)
-		self.xml.signal_autoconnect(self)
-		
-		self.widget = self.xml.get_widget(windowname)
-
-	def __getattr__(self, name): 
-		"""Allow glade widgets to be acessed as attributes."""
-
-		widget = self.xml.get_widget(name)
-		if widget is None:
-			raise AttributeError, "widget %s not found" % name
-
-		# cache reference
-		setattr(self, name, widget)
-
-		return widget
-
-
-class GladeApp(GladeWindow):
-	"""Base class for glade based applications."""
-
-	def main(self):
-		"""Enter main loop."""
-
-		gtk.main()
-
-	def quit(self, *args):
-		"""Quit main loop."""
-
-		gtk.main_quit()
-				
-	def open(self, title = None, parent = None, filters = None, folder = None):
-		"""Display a file open dialog."""
-
-		# See http://www.pygtk.org/pygtk2tutorial/sec-FileChoosers.html
-
-		dialog = gtk.FileChooserDialog(
-				title,
-				parent,
-				gtk.FILE_CHOOSER_ACTION_OPEN,
-				(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK),
-		)
-		dialog.set_default_response(gtk.RESPONSE_OK)
-
-		if filters is not None:
-			for name, patterns in filters:
-				filter = gtk.FileFilter()
-				filter.set_name(name)
-				for pattern in patterns:
-					filter.add_pattern(pattern)
-				dialog.add_filter(filter)
-
-		if folder is not None:
-				dialog.set_current_folder(os.path.abspath(folder))
-
-		response = dialog.run()
-		if response == gtk.RESPONSE_OK:
-			path = dialog.get_filename()
-		else:
-			path = None
-
-		dialog.destroy()
-		return path
-
-
-class MainApp(GladeApp):
+class MainApp(glade.GladeApp):
 
 	def __init__(self):
-		GladeApp.__init__(self, "./ui/main.glade", "main_window")
+		glade.GladeApp.__init__(self, "./ui/main.glade", "main_window")
 		
 		self.factory = aterm.Factory()
 		self.term = self.factory.parse("Module([])")
@@ -147,8 +63,4 @@ class MainApp(GladeApp):
 	def on_main_window_destroy(self, event):
 		self.quit()
 
-
-if __name__ == '__main__':
-	app = MainApp()
-	app.main()
 
