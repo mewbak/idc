30e03aa9a91ce5905d49de58dd4226dd8569bb59
José Fonseca <jrfonseca@users.sourceforge.net>
2006-05-18 22:03:48 +0000

    Remove extraneous self argument.

--- aterm/visitor.py
+++ aterm/visitor.py
@@ -14,33 +14,33 @@ class Visitor:
 		pass
 
 	def visitLit(self, term, *args, **kargs):
-		return self.visitTerm(self, term, *args, **kargs)
+		return self.visitTerm(term, *args, **kargs)
 		
 	def visitInt(self, term, *args, **kargs):
-		return self.visitLit(self, term, *args, **kargs)
+		return self.visitLit(term, *args, **kargs)
 
 	def visitReal(self, term, *args, **kargs):
-		return self.visitLit(self, term, *args, **kargs)
+		return self.visitLit(term, *args, **kargs)
 
 	def visitStr(self, term, *args, **kargs):
-		return self.visitLit(self, term, *args, **kargs)
+		return self.visitLit(term, *args, **kargs)
 
 	def visitWildcard(self, term, *args, **kargs):
-		return self.visitTerm(self, term, *args, **kargs)
+		return self.visitTerm(term, *args, **kargs)
 
 	def visitVar(self, term, *args, **kargs):
 		return self.visitTerm(term, *args, **kargs)
 	
 	def visitList(self, term, *args, **kargs):
-		return self.visitTerm(self, term, *args, **kargs)
+		return self.visitTerm(term, *args, **kargs)
 
 	def visitNilList(self, term, *args, **kargs):
-		return self.visitList(self, term, *args, **kargs)
+		return self.visitList(term, *args, **kargs)
 
 	def visitConsList(self, term, *args, **kargs):
-		return self.visitList(self, term, *args, **kargs)
+		return self.visitList(term, *args, **kargs)
 
 	def visitAppl(self, term, *args, **kargs):
-		return self.visitTerm(self, term, *args, **kargs)
+		return self.visitTerm(term, *args, **kargs)
 
 
