d6c1535d3d2dfc9f44ae8cebf55311e29e5d1a0d
José Fonseca <jrfonseca@users.sourceforge.net>
2006-06-07 11:33:37 +0000

    Revamp the CFG generation.

--- ir/cfg.py
+++ ir/cfg.py
@@ -9,6 +9,25 @@ from transf import *
 from ir import pprint2
 
 
+
+#######################################################################
+# Utilities
+
+class Counter(base.Transformation):
+
+	def __init__(self, value = 1):
+		base.Transformation.__init__(self)
+		self.value = value
+
+	def apply(self, term, context):
+		term = term.factory.makeInt(self.value)
+		self.value += 1
+		return term
+
+
+#######################################################################
+# Statement matching
+
 matchStmtName \
 	= match.Str('VarDef') \
 	| match.Str('FuncDef') \
@@ -24,120 +43,127 @@ matchStmtName \
 	| match.Str('NoStmt') \
 	| match.Str('Ret')
 
-
-class Counter(base.Transformation):
-
-	def __init__(self, value = 1):
-		base.Transformation.__init__(self)
-		self.value = value
-
-	def apply(self, term, context):
-		term = term.factory.makeInt(self.value)
-		self.value += 1
-		return term
-
-
-def AnnotateId():
-	return annotation.Set(build.Str('Id'), Counter())
-
-
-matchLabel = match.Appl(match.Str('Label'), base.ident)
-
 matchStmt = match.Appl(matchStmtName, base.ident)
 
-collectStmts = unify.CollectAll(matchStmt)
 
-markStmts = traverse.TopDown(combine.Try(matchStmt & AnnotateId()))
+#######################################################################
+# Statements IDs
 
+stmtIdAnno = build.Str('StmtId')
 
+getStmtId = annotation.Get(stmtIdAnno)
+SetStmtId = lambda id: annotation.Set(stmtIdAnno, id)
 
-this = build.Var("this")
-next = build.Var("next")
-cont = build.Var("cont")
-brek = build.Var("brek")
-retn = build.Var("retn")
-
-
-def Edge(src, dst):
-	return build.List((src, dst))
+MarkStmtsIds = lambda: traverse.TopDown(combine.Try(matchStmt & SetStmtId(Counter(1))))
 
 
-stmtsFlow = base.Proxy()
+#######################################################################
+# Statements Flow
 
-stmtFlow = base.Proxy()
-stmtFlow.subject = debug.Dump() & parse.Rule('''
-	Assign(*) -> [[<id>,next]] |
-	Label(_) -> [[<id>,next]] |
-	If(_,true,NoStmt) -> <concat(![[<id>,next],[<id>,true]],<stmtFlow> true)> |
-	If(_,true,false) -> <concat(![[<id>,true],[<id>,false]],<stmtFlow> true, <stmtFlow> false)> |
-	NoStmt -> [[<id>,next]] |
-	Ret(*) -> [[<id>,next]]
+stmtFlowAnno = build.Str('StmtFlow')
+
+getStmtFlow = annotation.Get(stmtFlowAnno)
+SetStmtFlow = lambda flows: annotation.Set(stmtFlowAnno, flows)
+SetStmtFlows = lambda *stmts: SetStmtFlow(build.List([stmt & getStmtId for stmt in stmts]))
+
+
+#######################################################################
+# Flow Traversal
+
+markStmtsFlow = base.Proxy()
+
+markStmtFlow = base.Proxy()
+
+markStmtFlow.subject = parse.Rule('''
+	Assign(*) 
+		-> <SetStmtFlows(!next)>
+|	Label(*) 
+		-> <SetStmtFlows(!next)>
+|	If(_, true, NoStmt) 
+		-> <SetStmtFlows(!next, !true); ~_(_, <markStmtFlow>, _) >
+|	If(_, true, false) 
+		-> <SetStmtFlows(!true, !false); ~_(_, <markStmtFlow>, <markStmtFlow>) >
+|	NoStmt
+		-> <SetStmtFlows(!next)>
+|	Ret(*)
+		-> <SetStmtFlows(!next)>
 ''')
 
-stmtsFlow.subject \
-	= scope.Scope(
-			match.nil \
-				& build.nil \
-			| match.Cons(
-				match.Var("head"), 
-				match.Var("tail") & (project.head | build.Var("next")) & match.Var("following")
-			) \
-				& lists.Concat(
-					scope.With(build.Var("head") & stmtFlow, next=build.Var("following")), 
-					build.Var("tail") & stmtsFlow
-				)
-		, ['head', 'tail', 'following'])
+markStmtsFlow.subject \
+	= match.nil \
+	| scope.With(
+		traverse.Cons(
+			scope.With(
+				markStmtFlow,
+				next = build._.following
+			),
+			markStmtsFlow
+		),
+		following = project.tail & (project.head | build._.next)
+	)
+
+endOfModule = build._.NoStmt() & SetStmtId(build.Int(0))
+
+markModuleFlow \
+	= traverse._.Module( 
+		scope.With(
+				markStmtsFlow, 
+				next=endOfModule, 
+				cont=endOfModule, 
+				brek=endOfModule, 
+				retn=endOfModule
+		)
+	)
+
+markFlow = markModuleFlow
+
+MarkFlow = lambda: MarkStmtsIds() & markFlow
 
 
-endOfModule = build.Pattern("NoStmt{Id(0)}") 
+#######################################################################
+# Graph Generation
 
-moduleEdges \
-	= match.Pattern("Module(stmts)") \
-		& scope.With(build.Var("stmts") & stmtsFlow, next=endOfModule, cont=endOfModule, brek=endOfModule, retn=endOfModule)
+makeNodeId = strings.ToStr()
 
-	#| match.Str('Branch(label)') & build.List((Edge(this,FindLabel(label)))) \
-		#| match.Pattern("If(*,true,false)") & Concat(Concat(build.Pattern("[Edge(this,next)]", build.Pattern("[this, true, next]") & stmtsFlow), build.Pattern("[this, false, next]") & stmtsFlow)
-		#| match.Pattern("While(*,block)") & Concat(build.Pattern("[Edge(this,next)]", build.Pattern("[this, next]") & stmtsFlow))
-	#| match.Str('While') \
-	#| match.Str('Label') \
-	#| match.Str('Block') \
-	#| match.Str('Break') \
-	#| match.Str('Continue') \
-	#| match.Str('Ret')
+renderBox = base.Adaptor(lambda term, context: term.factory.makeStr(box.box2text(term)))
 
+makeNodeLabel = parse.Rule('''
+	Assign(*)
+		-> < pprint2.stmt; renderBox >
+|	Label(*)
+		-> < pprint2.stmt; renderBox >
+|	If(cond,_,_)
+		-> < <pprint2.expr> cond; renderBox >
+|	Ret(*)
+		-> < pprint2.stmt; renderBox >
+|	NoStmt(*)
+		-> ""
+|	_ 
+		-> "..."
+''')
 
-makeNodeId \
-	= annotation.Get(build.Str('Id')) & strings.ToStr()
+makeNode = build._.Node(getStmtId & makeNodeId, makeNodeLabel)
+makeNodes = unify.CollectAll(makeNode)
 
-box2text = base.Adaptor(
-		lambda term, context: term.factory.makeStr(box.box2text(term))
+makeNodeEdges = scope.With(
+	getStmtFlow & traverse.Map(
+		build._.Edge(build._.src, makeNodeId)
+	),
+	src = getStmtId & makeNodeId
 )
 
-makeNodeLabel = parse.Transf('''
-		?Assign(*); pprint2.stmt; box2text +
-		?Label(*); pprint2.stmt; box2text +
-		?If(cond,_,_); <pprint2.expr> cond; box2text +
-		?Ret(*); pprint2.stmt; box2text +
-		?NoStmt(*); !"" +
-		! "..."
-''')
+makeEdges \
+	= unify.CollectAll(makeNodeEdges) \
+	& unify.Foldr(
+	build.nil,
+	lists.Concat
+)
 
-makeNode = build.Appl("Node", (makeNodeId, makeNodeLabel))
-makeNodes = traverse.Map(makeNode)
+makeGraph = build._.Graph(makeNodes, makeEdges)
 
 
-makeEdge = parse.Rule('''
-	[src, dst] -> Edge(<<makeNodeId> src>, <<makeNodeId> dst>)
-''')
-makeEdges = traverse.Map(makeEdge)
-
-collectFlows = moduleEdges
-
-makeGraph = parse.Transf('''
-		markStmts;
-		!Graph(<collectStmts; makeNodes>, <collectFlows; makeEdges>)
-''')
-
+#######################################################################
+# Dot output
 
 makeDot = parse.Rule(r'''
 		Graph(nodes, edges)
@@ -156,6 +182,8 @@ makeDot = parse.Rule(r'''
 
 makeDot = traverse.BottomUp(makeDot)
 
+
+
 if __name__ == '__main__':
 	import aterm.factory
 	import sys
@@ -163,10 +191,14 @@ if __name__ == '__main__':
 	for arg in sys.argv[1:]:
 		term = factory.readFromTextFile(file(arg, 'rt'))
 
-		#print markStmts(term)
-		#print ( markStmts & collectFlows & makeEdges )(term)
-		#print ( markStmts & collectStmts & makeNodes )(term)
-
+		term = MarkStmtsIds() (term)
+		print term
+		term = markFlow (term)
+		print term
+		
+		print makeNodes (term)
+		print makeEdges (term)
+		
 		term = makeGraph(term)
 		print term
 		
