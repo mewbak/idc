cdf789e33c4ecdd3d145448b23b9999c64123e13
José Fonseca <jrfonseca@users.sourceforge.net>
2007-06-25 23:37:51 +0000

    Complete the condition codes.

--- machine/pentium/control.py
+++ machine/pentium/control.py
@@ -3,6 +3,7 @@
 
 from transf import parse
 from machine.pentium.common import *
+from machine.pentium.conditions import *
 
 
 parse.Transfs('''
@@ -10,23 +11,53 @@ parse.Transfs('''
 asmJMP =
 	[Ref(addr)] -> [GoTo(addr)]
 
+
 AsmJcc(cond) =
 	[Ref(addr)] -> [If(<cond>,GoTo(addr),NoStmt)]
 
-asmJA = AsmJcc(LAnd(LNot(cf),LNot(zf)))
-asmJE = AsmJcc(zf)
-asmJLE = AsmJcc(LAnd(LNot(zf),LNotEq(sf,of)))
-asmJBE = AsmJcc(LOr(cf,zf))
-asmJNE = AsmJcc(LNot(zf))
-asmJG = AsmJcc(LAnd(LNot(zf),LEq(sf,of)))
-#TODO: complete remaining
+asmJA    = AsmJcc(ccA   )
+asmJAE   = AsmJcc(ccAE  )
+asmJB    = AsmJcc(ccB   )
+asmJBE   = AsmJcc(ccBE  )
+asmJC    = AsmJcc(ccC   )
+asmJCXZ  = AsmJcc(ccCXZ )
+asmJECXZ = AsmJcc(ccECXZ)
+asmJE    = AsmJcc(ccE   )
+asmJG    = AsmJcc(ccG   )
+asmJGE   = AsmJcc(ccGE  )
+asmJL    = AsmJcc(ccL   )
+asmJLE   = AsmJcc(ccLE  )
+asmJNA   = AsmJcc(ccNA  )
+asmJNAE  = AsmJcc(ccNAE )
+asmJNB   = AsmJcc(ccNB  )
+asmJNBE  = AsmJcc(ccNBE )
+asmJNC   = AsmJcc(ccNC  )
+asmJNE   = AsmJcc(ccNE  )
+asmJNG   = AsmJcc(ccNG  )
+asmJNGE  = AsmJcc(ccNGE )
+asmJNL   = AsmJcc(ccNL  )
+asmJNLE  = AsmJcc(ccNLE )
+asmJNO   = AsmJcc(ccNO  )
+asmJNP   = AsmJcc(ccNP  )
+asmJNS   = AsmJcc(ccNS  )
+asmJNZ   = AsmJcc(ccNZ  )
+asmJO    = AsmJcc(ccO   )
+asmJP    = AsmJcc(ccP   )
+asmJPE   = AsmJcc(ccPE  )
+asmJPO   = AsmJcc(ccPO  )
+asmJS    = AsmJcc(ccS   )
+asmJZ    = AsmJcc(ccZ   )
+
 
 asmCALL =
 	[Ref(addr)] -> [Assign(Void, NoExpr, Call(addr,[]))]
 
+
 asmRET =
 	[] -> [Ret(Void, NoExpr)]
 
+
 ''')
+
 
 
--- machine/pentium/data.py
+++ machine/pentium/data.py
@@ -3,7 +3,7 @@
 
 from transf import parse
 from machine.pentium.common import *
-
+from machine.pentium.conditions import *
 
 parse.Transfs('''
 
@@ -14,6 +14,73 @@ asmMOVB = AsmMOV(!8)
 asmMOVW = AsmMOV(!16)
 asmMOVL = AsmMOV(!32)
 
+
+AsmCMOVcc(size,cond) =
+	[dst,src] -> [If(<cond>,Assign(<Word(size)>,dst,src),NoStmt)]
+
+asmCMOVAW   = AsmCMOVcc(!16, ccA   )
+asmCMOVAEW  = AsmCMOVcc(!16, ccAE  )
+asmCMOVBW   = AsmCMOVcc(!16, ccB   )
+asmCMOVBEW  = AsmCMOVcc(!16, ccBE  )
+asmCMOVCW   = AsmCMOVcc(!16, ccC   )
+asmCMOVEW   = AsmCMOVcc(!16, ccE   )
+asmCMOVGW   = AsmCMOVcc(!16, ccG   )
+asmCMOVGEW  = AsmCMOVcc(!16, ccGE  )
+asmCMOVLW   = AsmCMOVcc(!16, ccL   )
+asmCMOVLEW  = AsmCMOVcc(!16, ccLE  )
+asmCMOVNAW  = AsmCMOVcc(!16, ccNA  )
+asmCMOVNAEW = AsmCMOVcc(!16, ccNAE )
+asmCMOVNBW  = AsmCMOVcc(!16, ccNB  )
+asmCMOVNBEW = AsmCMOVcc(!16, ccNBE )
+asmCMOVNCW  = AsmCMOVcc(!16, ccNC  )
+asmCMOVNEW  = AsmCMOVcc(!16, ccNE  )
+asmCMOVNGW  = AsmCMOVcc(!16, ccNG  )
+asmCMOVNGEW = AsmCMOVcc(!16, ccNGE )
+asmCMOVNLW  = AsmCMOVcc(!16, ccNL  )
+asmCMOVNLEW = AsmCMOVcc(!16, ccNLE )
+asmCMOVNOW  = AsmCMOVcc(!16, ccNO  )
+asmCMOVNPW  = AsmCMOVcc(!16, ccNP  )
+asmCMOVNSW  = AsmCMOVcc(!16, ccNS  )
+asmCMOVNZW  = AsmCMOVcc(!16, ccNZ  )
+asmCMOVOW   = AsmCMOVcc(!16, ccO   )
+asmCMOVPW   = AsmCMOVcc(!16, ccP   )
+asmCMOVPEW  = AsmCMOVcc(!16, ccPE  )
+asmCMOVPOW  = AsmCMOVcc(!16, ccPO  )
+asmCMOVSW   = AsmCMOVcc(!16, ccS   )
+asmCMOVZW   = AsmCMOVcc(!16, ccZ   )
+
+asmCMOVAL   = AsmCMOVcc(!32, ccA   )
+asmCMOVAEL  = AsmCMOVcc(!32, ccAE  )
+asmCMOVBL   = AsmCMOVcc(!32, ccB   )
+asmCMOVBEL  = AsmCMOVcc(!32, ccBE  )
+asmCMOVCL   = AsmCMOVcc(!32, ccC   )
+asmCMOVEL   = AsmCMOVcc(!32, ccE   )
+asmCMOVGL   = AsmCMOVcc(!32, ccG   )
+asmCMOVGEL  = AsmCMOVcc(!32, ccGE  )
+asmCMOVLL   = AsmCMOVcc(!32, ccL   )
+asmCMOVLEL  = AsmCMOVcc(!32, ccLE  )
+asmCMOVNAL  = AsmCMOVcc(!32, ccNA  )
+asmCMOVNAEL = AsmCMOVcc(!32, ccNAE )
+asmCMOVNBL  = AsmCMOVcc(!32, ccNB  )
+asmCMOVNBEL = AsmCMOVcc(!32, ccNBE )
+asmCMOVNCL  = AsmCMOVcc(!32, ccNC  )
+asmCMOVNEL  = AsmCMOVcc(!32, ccNE  )
+asmCMOVNGL  = AsmCMOVcc(!32, ccNG  )
+asmCMOVNGEL = AsmCMOVcc(!32, ccNGE )
+asmCMOVNLL  = AsmCMOVcc(!32, ccNL  )
+asmCMOVNLEL = AsmCMOVcc(!32, ccNLE )
+asmCMOVNOL  = AsmCMOVcc(!32, ccNO  )
+asmCMOVNPL  = AsmCMOVcc(!32, ccNP  )
+asmCMOVNSL  = AsmCMOVcc(!32, ccNS  )
+asmCMOVNZL  = AsmCMOVcc(!32, ccNZ  )
+asmCMOVOL   = AsmCMOVcc(!32, ccO   )
+asmCMOVPL   = AsmCMOVcc(!32, ccP   )
+asmCMOVPEL  = AsmCMOVcc(!32, ccPE  )
+asmCMOVPOL  = AsmCMOVcc(!32, ccPO  )
+asmCMOVSL   = AsmCMOVcc(!32, ccS   )
+asmCMOVZL   = AsmCMOVcc(!32, ccZ   )
+
+
 AsmPUSH(size) =
 	[src] -> [
 		Assign(<Word(size)>,<Reg(!"esp")>,
@@ -23,6 +90,7 @@ AsmPUSH(size) =
 
 asmPUSHW = AsmPUSH(!8)
 asmPUSHL = AsmPUSH(!16)
+
 
 AsmPOP(size) =
 	[dst] -> [
