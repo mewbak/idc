bdecf7ac2fdf2b13bdeee2c331a47733726a1b9d
José Fonseca <jrfonseca@users.sourceforge.net>
2007-07-13 00:51:58 +0900

    Partially implement PUSHF/POPF.

--- machine/pentium/flag.py
+++ machine/pentium/flag.py
@@ -3,6 +3,7 @@
 
 from transf import parse
 from machine.pentium.common import *
+from machine.pentium.data import *
 
 
 parse.Transfs('''
@@ -40,8 +41,29 @@ asmSAHF = [] -> [
 		type := UWord(!8)
 
 
-# FIXME: PUSHF/PUSHFD
-# FIXME: POPF/POPFD
+asmPUSHFL = [] -> [
+		Binary(Or(type), Cond(<cf>, Lit(type,0x01), Lit(type,0)),
+		Binary(Or(type), Lit(type,0x02),
+		Binary(Or(type), Cond(<pf>, Lit(type,0x04), Lit(type,0)),
+		Binary(Or(type), Cond(<af>, Lit(type,0x10), Lit(type,0)),
+		Binary(Or(type), Cond(<zf>, Lit(type,0x40), Lit(type,0)),
+			Cond(<sf>, Lit(type,0x80), Lit(type,0)))))))
+		# FIXME: incomplete
+	] ; asmPUSHL
+		where type := UWord(!32)
+
+
+asmPOPFL = [] -> [
+		<asmPOPL [tmp] >,
+		Assign(type, <cf>, Binary(And(type), tmp, Lit(type,0x01))),
+		Assign(type, <pf>, Binary(And(type), tmp, Lit(type,0x04))),
+		Assign(type, <af>, Binary(And(type), tmp, Lit(type,0x10))),
+		Assign(type, <zf>, Binary(And(type), tmp, Lit(type,0x40))),
+		Assign(type, <sf>, Binary(And(type), tmp, Lit(type,0x80)))
+		# FIXME: incomplete
+	] where
+		type := UWord(!32) ;
+		tmp := temp
 
 
 asmSTI = [] -> [Assign(Bool, <if_>, Lit(Bool,1))]
